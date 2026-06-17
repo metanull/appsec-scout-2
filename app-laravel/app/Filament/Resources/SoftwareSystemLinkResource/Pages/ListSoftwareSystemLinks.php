040fb1ee82fc84ddf5ec24fc8c7b181b4f874555
Pascal HAVELANGE <havelangep@hotmail.com>
2026-06-17 17:19:24 +0200

    Fix (filament): Fix absence of the Create buttons
    In Filament 5, create buttons hav eto be added explicitly, unlike in Filament 3

--- app-laravel/app/Filament/Resources/SoftwareSystemLinkResource/Pages/ListSoftwareSystemLinks.php
+++ app-laravel/app/Filament/Resources/SoftwareSystemLinkResource/Pages/ListSoftwareSystemLinks.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\SoftwareSystemLinkResource\Pages;
 
 use App\Filament\Resources\SoftwareSystemLinkResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListSoftwareSystemLinks extends ListRecords
 {
     protected static string $resource = SoftwareSystemLinkResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
